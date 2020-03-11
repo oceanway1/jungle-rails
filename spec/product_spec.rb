b1850a8cf73c3aecc4efb4673cf622af9603c09e
oceanway1 <oceanway1@yahoo.ca>
2020-03-11 00:11:38 +0000

    test when you user enter invalid email, password

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    # validation tests/examples here
+    it 'should create a product if all of the validations are true' do
+      @category = Category.new(name: "Testing")
+      @product = Product.new(name: "Nike", price_cents: 500, quantity: 20, :category => @category)
+      @product.valid?
+      expect(@product.errors.full_messages).not_to eql("can\'t be blank")
+      end
+    it 'should not create a product if the name is missing' do
+      @category = Category.new(name: "Testing")
+      @product = Product.new(price_cents: 500, quantity: 20, :category => @category)
+      @product.valid?
+      expect(@product.errors.full_messages).to eql(["Name can\'t be blank"])
+      end
+    it 'should not create a product if the quantity is missing' do
+      @category = Category.new(name: "Testing")
+      @product = Product.new(name: "Nike", price_cents: 500, :category => @category)
+      @product.valid?
+      expect(@product.errors.full_messages).to eql(["Quantity can\'t be blank"])
+      end
+  
+    it 'should not create a product if the price is missing' do
+      @category = Category.new(name: "Testing")
+      @product = Product.new(name: "Nike", quantity: 20, :category => @category)
+      @product.valid?
+      expect(@product.errors.full_messages).to eql(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
+      end
+
+    it 'should not create a product if the category is missing' do
+      @category = Category.new(name: "Testing")
+      @product = Product.new(name: "Nike", price_cents: 500, quantity: 20)
+      @product.valid?
+      expect(@product.errors.full_messages).to eql(["Category can\'t be blank"])    
+    end
+  end
 end
